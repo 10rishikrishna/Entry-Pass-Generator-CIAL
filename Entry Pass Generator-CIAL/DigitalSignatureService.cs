a7fe500fbc563c05aff6fe4b7aca1928985479a0
Rishi Krishna <bss10a04rishi@gmail.com>
2026-04-08 16:03:44 +0530

    Add summary documentation for DigitalSignatureData
    Added summary documentation for DigitalSignatureData class.

--- Entry Pass Generator-CIAL/DigitalSignatureService.cs
+++ Entry Pass Generator-CIAL/DigitalSignatureService.cs
@@ -7,6 +7,7 @@ using System.Text.Json.Serialization;
 namespace CIAL.Shared.DigitalSignature
 {
     /// <summary>
+
     /// Digital Signature Model - contains all signature information
     /// </summary>
     public class DigitalSignatureData
